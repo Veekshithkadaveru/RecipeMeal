f5f6b89c34b002a4616f5f5445a1aac111205e57
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2025-02-12 22:37:47 +0530

    Define MealsResponse data class for API response

--- app/src/main/java/com/example/recipemeal/data/Meal.kt
+++ app/src/main/java/com/example/recipemeal/data/Meal.kt
@@ -2,4 +2,8 @@ package com.example.recipemeal.data
 
 import com.google.gson.annotations.SerializedName
 
-data class Meal(@SerializedName("meals") val meals: List<Meal>)
+data class Meal(
+    @SerializedName("idMeal") val id: String,
+    @SerializedName("strMeal") val name: String,
+    @SerializedName("strMealThumb") val thumbnail: String
+)
