ad5063eba1ba9f3f77e78981c5928092cecc731c
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2025-02-12 22:41:33 +0530

    Define MealDetail data class with serialized names

---
+++ app/src/main/java/com/example/recipemeal/data/MealDetail.kt
@@ -0,0 +1,20 @@
+package com.example.recipemeal.data
+
+import com.google.gson.annotations.SerializedName
+
+data class MealDetail(
+    @SerializedName("idMeal") val id: String,
+    @SerializedName("strMeal") val name: String,
+    @SerializedName("strMealThumb") val thumbnail: String,
+    @SerializedName("strInstructions") val instructions: String,
+    @SerializedName("strIngredient1") val ingredient1: String?,
+    @SerializedName("strIngredient2") val ingredient2: String?,
+    @SerializedName("strIngredient3") val ingredient3: String?,
+    @SerializedName("strIngredient4") val ingredient4: String?,
+    @SerializedName("strIngredient5") val ingredient5: String?,
+    @SerializedName("strIngredient6") val ingredient6: String?,
+    @SerializedName("strIngredient7") val ingredient7: String?,
+    @SerializedName("strIngredient8") val ingredient8: String?,
+    @SerializedName("strIngredient9") val ingredient9: String?,
+    @SerializedName("strIngredient10") val ingredient10: String?,
+)
