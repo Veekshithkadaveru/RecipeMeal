2d5acc6cb804a7b9dc7318b30f4be467d8cbe18e
Veekshith Kadaveru <veekshithkadaveru4@gmail.com>
2025-02-14 22:53:27 +0530

    Create Meal Repository

---
+++ app/src/main/java/com/example/recipemeal/Repository.kt
@@ -0,0 +1,27 @@
+package com.example.recipemeal
+
+import com.example.recipemeal.data.CategoriesResponse
+import com.example.recipemeal.data.MealDetailsResponse
+import com.example.recipemeal.data.MealsResponse
+import com.example.recipemeal.model.api.ApiService
+import javax.inject.Inject
+
+class Repository @Inject constructor(private val apiService:ApiService) {
+
+    suspend fun getCategories():CategoriesResponse{
+       return apiService.getCategories()
+    }
+
+    suspend fun searchMeals(query:String): MealsResponse {
+        return apiService.searchMeals(query )
+    }
+
+    suspend fun getMealById(id:String):MealDetailsResponse{
+        return apiService.getMealById(id)
+    }
+
+    suspend fun filterByCategoryResponse(category:String):MealsResponse{
+        return apiService.filterByCategory(category)
+    }
+
+}
